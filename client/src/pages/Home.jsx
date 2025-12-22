17c0e5105049076c92eb9efd362f4b42be455a4b
Karandeep SIngh <64605839+karandhayal@users.noreply.github.com>
2025-12-22 19:57:36 +0530

    v2

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const { data } = await axios.get('http://localhost:5000/api/products');
+        const { data } = await axios.get('/api/products');
         setProducts(data);
         setLoading(false);
       } catch (error) {
